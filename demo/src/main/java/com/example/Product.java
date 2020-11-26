20e0416900fd323475896c3096705e4e6f1cfff2
xingchunwang <xingchunwang@gmail.com>
2020-11-26 19:43:16 +0800

    save

---
+++ demo/src/main/java/com/example/Product.java
@@ -0,0 +1,22 @@
+package com.example;
+
+public class Product {
+    String Id;
+    String Name;
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getId() {
+        return this.Id;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+}
